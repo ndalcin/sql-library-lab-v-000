e2eafc90868c1c45f6792410daba7481861af795
ndalcin <dalcin.nicole@gmail.com>
2017-10-03 22:10:03 -0400

    Done.

--- lib/querying.rb
+++ lib/querying.rb
@@ -9,7 +9,8 @@ end
 def select_name_and_motto_of_char_with_longest_motto
   "SELECT name, motto
   FROM characters
-  ORDER BY LENGTH(motto) DESC LIMIT 1"
+  ORDER BY LENGTH(motto) DESC
+  LIMIT 1"
 end
 
 
@@ -17,7 +18,8 @@ def select_value_and_count_of_most_prolific_species
   "SELECT species, COUNT(species)
   FROM characters
   GROUP BY species
-  ORDER BY COUNT(species) DESC LIMIT 1"
+  ORDER BY COUNT(species) DESC
+  LIMIT 1"
 end
 
 def select_name_and_series_subgenres_of_authors
